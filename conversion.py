92364b60e1b66e95282406406810979e94188620
daringanitch <dganitch@gmail.com>
2023-01-26 12:21:07 -0800

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,32 +1,51 @@
 import os
+import concurrent.futures
+from io import StringIO
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
+from pdfminer.pdfpage import PDFPage, create_pages
 from gtts import gTTS
+from playsound import playsound
 
-def pdf_to_mp3(pdf_file, mp3_file, store_location):
-  # Open the PDF file
-  with open(pdf_file, 'rb') as file:
+# Open the PDF file
+with open('document.pdf', 'rb') as file:
     # Create a PDF resource manager object
-    rsrcmgr = PDFResourceManager()
-    # Set up a StringIO object to hold the PDF text
-    text_io = StringIO()
-    # Set up a PDF converter object
-    converter = TextConverter(rsrcmgr, text_io, laparams=LAParams())
+    resource_manager = PDFResourceManager()
+    
+    # Create a string buffer
+    buffer = StringIO()
+    
+    # Create a PDF converter object
+    converter = TextConverter(resource_manager, buffer, codec='utf-8', laparams=LAParams())
+    
     # Create a PDF interpreter object
-    interpreter = PDFPageInterpreter(rsrcmgr, converter)
-    # Process each page of the PDF
-    for page in PDFPage.get_pages(file, pagenos=set(), maxpages=0, password="", caching=True, check_extractable=True):
-      interpreter.process_page(page)
-    # Get the text from the StringIO object
-    text = text_io.getvalue()
-    # Generate an MP3 file from the text
-    tts = gTTS(text)
-    # Save the MP3 file to the specified location
-    tts.save(os.path.join(store_location, mp3_file))
-
-# Example usage
-pdf_to_mp3('document.pdf', 'audio.mp3', '/path/to/store/location')
-
+    interpreter = PDFPageInterpreter(resource_manager, converter)
+    
+    # Extract the pages from the PDF
+    pages = create_pages(file)
+    
+    # Convert the pages in parallel
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(interpreter.process_page, page) for page in pages]
+        concurrent.futures.wait(futures)
+    
+    # Close the converter and buffer
+    converter.close()
+    buffer.close()
+    
+    # Get the text from the buffer
+    text = buffer.getvalue()
+    
+    # Convert the text to speech
+    audio = gTTS(text)
+    
+    # Save the audio to an MP3 file
+    with open('document.mp3', 'wb') as audio_file:
+        audio.write_to_fp(audio_file)
+    
+    # Play the audio file
+    playsound('document.mp3')
+    
+    # Delete the audio file
+    os.unlink('document.mp3')
